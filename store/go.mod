7d68dfe1dc16a3b5de75e0c092bebf71868806cc
Yuri Vinicius <yuriprodigio@hotmail.com>
2025-02-06 10:49:53 -0300

    Conectando ao banco de dados postgres

---
+++ store/go.mod
@@ -0,0 +1,5 @@
+module golang/store
+
+go 1.23.4
+
+require github.com/lib/pq v1.10.9
--- store/main.go
+++ store/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 type Product struct {
+	Id          int
 	Name        string
 	Description string
 	Price       float64
@@ -20,11 +24,40 @@ func main() {
 }
 
 func index(writer http.ResponseWriter, request *http.Request) {
-	produtos := []Product{
-		{"Camiseta", "Azul, bem bonita", 39, 5},
-		{"Tênis", "Confortável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
+	db := connectDataBase()
+
+	produtosDb, err := db.Query("select * from products")
+
+	defer db.Close()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produtos := []Product{}
+
+	for produtosDb.Next() {
+
+		var product Product
+
+		err = produtosDb.Scan(&product.Id, &product.Name, &product.Description, &product.Price, &product.Quantity)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produtos = append(produtos, product)
 	}
 
 	templates.ExecuteTemplate(writer, "Index", produtos)
+}
+
+func connectDataBase() *sql.DB {
+	connection := "user=postgres dbname=store password=1234 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
 }
